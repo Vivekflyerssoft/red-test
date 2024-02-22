9eedf334c5af72d56b3d52f863c265a5dd57ef8f
vivek-flyers <vivek.n@flyerssoft.com>
2024-02-23 00:24:16 +0530

    Refactoring - User_Should_Not_Be_Allowed_To_Add_More_Than_5_Beneficiary

---
+++ RedTest.Domain/Beneficiaries.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+
+namespace RedTest.Domain;
+
+public class Beneficiaries : IEnumerable<Beneficiary>
+{
+    List<Beneficiary> beneficiaryList = new List<Beneficiary>();
+
+    public Beneficiary this[int index]
+    {
+        get => beneficiaryList[index];
+        set => beneficiaryList.Insert(index, value);
+    }
+
+    public IEnumerator<Beneficiary> GetEnumerator()
+    {
+        return beneficiaryList.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public bool Add(Beneficiary beneficiary)
+    {
+        if (beneficiaryList.Count == 5)
+        {
+            return false;
+        }
+        beneficiaryList.Add(beneficiary);
+        return true;
+    }
+}
--- RedTest.Domain/User.cs
+++ RedTest.Domain/User.cs
@@ -1,20 +1,19 @@
 using System.Collections;
+using System.Linq;
+
 namespace RedTest.Domain;
 
 public class User
 {
     public User()
     {
-        Beneficiaries = new List<Beneficiary>();
+        Beneficiaries = new Beneficiaries();
     }
-    public ICollection<Beneficiary> Beneficiaries { get; private set; }
+
+    public Beneficiaries Beneficiaries { get; private set; }
 
     public bool AddBeneficiary(Beneficiary beneficiary)
     {
-        if(Beneficiaries.Count == 5){
-            return false;
-        }
-        Beneficiaries.Add(beneficiary);
-        return true;
+        return Beneficiaries.Add(beneficiary);
     }
 }
