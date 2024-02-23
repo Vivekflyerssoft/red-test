85cd27b382347bbb5520b1f45614e137f6129ed7
vivek-flyers <vivek.n@flyerssoft.com>
2024-02-23 16:45:41 +0530

    Refactor topup list to its own class

--- RedTest.Domain/Beneficiary.cs
+++ RedTest.Domain/Beneficiary.cs
@@ -1,43 +1,44 @@
-using System.Linq;
-
 namespace RedTest.Domain;
 
 public class Beneficiary
 {
     private const int VERIFIED_USER_BENEFICIARY_TOPUP_MAX_LIMIT = 500;
     private const int UNVERIFIED_USER_BENEFICIARY_TOPUP_MAX_LIMIT = 1000;
-    private readonly Dictionary<string, List<uint>> _topUps;
+    private readonly TopUpList _topUps;
 
     public string NickName { get; set; }
 
     public Beneficiary(string nickName)
     {
         NickName = nickName;
-        _topUps = new Dictionary<string, List<uint>>();
+        _topUps = new TopUpList();
     }
 
     public Result TopUp(uint withAmount, bool isVerifiedUser)
     {
-        var today = DateTime.Now;
-        string key = $"{today.Month}/{today.Year}";
-        if (_topUps.ContainsKey(key))
+        if (_topUps.IsRechargeDataAvailableForCurrentMonth)
         {
-            long amount = _topUps[key].Sum(x => x) + withAmount;
-            if (isVerifiedUser && amount > VERIFIED_USER_BENEFICIARY_TOPUP_MAX_LIMIT)
+            uint sumOfTopUpAmountForCurrentMonth = _topUps.GetCurrentMonthTotalTopUpAmount() + withAmount;
+            Result validationResult = Validate(sumOfTopUpAmountForCurrentMonth, isVerifiedUser);
+            if (!validationResult.IsSuccess)
             {
-                return ResultFactory.Error("TopUp failed, verified beneficiary top limit for the month is reached.");
+                return validationResult;
             }
-            else if (!isVerifiedUser && amount > UNVERIFIED_USER_BENEFICIARY_TOPUP_MAX_LIMIT)
-            {
-                return ResultFactory.Error("TopUp failed, unverified beneficiary top limit for the month is reached.");
-            }
-            _topUps[key].Add(withAmount);
         }
-        else
-        {
-            _topUps.Add(key, new List<uint> { withAmount });
-        }
+        _topUps.Add(withAmount);
         return ResultFactory.Success();
     }
 
+    private Result Validate(uint sumOfTopUpAmountForCurrentMonth, bool isVerifiedUser)
+    {
+        if (isVerifiedUser && sumOfTopUpAmountForCurrentMonth > VERIFIED_USER_BENEFICIARY_TOPUP_MAX_LIMIT)
+        {
+            return ResultFactory.Error("TopUp failed, verified beneficiary top limit for the month is reached.");
+        }
+        else if (!isVerifiedUser && sumOfTopUpAmountForCurrentMonth > UNVERIFIED_USER_BENEFICIARY_TOPUP_MAX_LIMIT)
+        {
+            return ResultFactory.Error("TopUp failed, unverified beneficiary top limit for the month is reached.");
+        }
+        return ResultFactory.Success();
+    }
 }
